f4d769584431a0cfdc1c29871781a63d0cd4106f
TopGek99 <arowe890@gmail.com>
2021-10-07 21:06:44 +1030

    some fixes

--- ft_memchr.c
+++ ft_memchr.c
@@ -1,19 +1,19 @@
 #include <stddef.h>
 
-void	*ft_memchr(void *s, int c, size_t n)
+void	*ft_memchr(const void *s, int c, size_t n)
 {
 	size_t	i;
 	char	*str;
 
-	str = s;
+	str = (char *)s;
 	i = 0;
 	while (str[i] && i < n)
 	{
 		if (str[i] == c)
-			return (s + i);
+			return ((void *)s + i);
 		i++;
 	}
 	if (c == '\0' && !(str[i]))
-		return (s + i);
+		return ((void *)s + i);
 	return (0);
 }
--- libft.h
+++ libft.h
@@ -1,6 +1,8 @@
 #ifndef LIBFT_H
 # define LIBFT_H
 
+#include <stddef.h>
+
 int		ft_isalpha(int c);
 int		ft_isdigit(int c);
 int		ft_isalnum(int c);
@@ -20,7 +22,7 @@ char	*ft_strdup(const char *src);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
-void	*ft_memchr(void *s, int c, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strnstr(const char *big, const char *little, size_t n);
 void	*ft_calloc(size_t nmemb, size_t size);
